18429edbf05a5f8d0e092052f82e86352924e836
vovkalucky <vladimir.inves@gmail.com>
2023-10-23 00:22:27 +0300

    add headless Chrome in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver():
+    options = Options()
+    options.add_argument('--headless')
     driver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service)
+    driver = webdriver.Chrome(service=driver_service, options=options)
 
     driver.maximize_window()  #вся ширина экрана
     yield driver  # все что до yield делается до теста
